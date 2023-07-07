f606e0e1684994c2cb5ca128ba2cfaa1fc892478
RicardoL1u <2018212874@bupt.edu.cn>
2023-07-07 02:55:15 +0000

    fea: text modifier

---
+++ DemoRe/text_modifier.py
@@ -0,0 +1,27 @@
+from flair.data import Sentence
+from flair.models import SequenceTagger
+from collections import defaultdict
+from copy import deepcopy
+class EntityAnonymizer():
+    def __init__(self, model_name):
+        """
+        model_name: the path to the model
+        could be downloaded from https://github.com/flairNLP/flair/#state-of-the-art-models
+        """
+        
+        self.model = SequenceTagger.load(model_name)
+    
+    def anonymize(self, text):
+        sentence = Sentence(text)
+        self.model.predict(sentence)
+        entity_tag_cnt = defaultdict(int)
+        anonymized_text = deepcopy(text)
+        for entity in sentence.get_spans('ner'):
+            anonymized_text = anonymized_text.replace(entity.text,'['+entity.tag+'_'+str(entity_tag_cnt[entity.tag])+']')
+            entity_tag_cnt[entity.tag] += 1
+        return anonymized_text
+    
+if __name__ == '__main__':
+    anonymizer = EntityAnonymizer('/data/lyt/models/flair/ner-english-ontonotes-large/pytorch_model.bin')
+    print(anonymizer.anonymize('George Washington went to Washington .'))
+        
