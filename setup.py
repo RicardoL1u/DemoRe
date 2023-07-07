e694220be4a9017923629bfbe74a7b561b185fef
RicardoL1u <2018212874@bupt.edu.cn>
2023-07-07 06:01:36 +0000

    fea: requires

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='DemoRe',
-    version='0.2',
+    version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='DemoRe',
     long_description=open('README.md').read(),
-    install_requires=['numpy'],
+    install_requires=[
+        'numpy'
+        'torch',
+        'transformers',
+        'flair',
+        'faiss-gpu',
+    ],
     url='https://github.com/RicardoL1u/DemoRe',
     author='RicardoL1u',
     author_email='ricardoliu@outlook.com'
