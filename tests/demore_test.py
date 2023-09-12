d6eb75c84c51b4ac5dfb9d55fb7c67cffa12ed9c
RicardoL1u <2018212874@bupt.edu.cn>
2023-09-12 13:21:26 +0000

    fea: uie dataset

--- tests/demore_test.py
+++ tests/demore_test.py
@@ -3,8 +3,8 @@ from DemoRe.demo_retriever import DemoRetriever
 import numpy as np
 import logging
 # load demos
-demos = json.load(open('tests/train.json','r'))
-demo_embs = np.load(open('tests/train_question_embs_company.npy','rb'))
+demos = json.load(open('../ALL_DATASETS_NEW/EE/ACE05/train.json','r'))
+demo_embs = np.load(open('../ALL_DATASETS_NEW/EE/ACE05/train_question_embs.npy','rb'))
 
 demo_retriever = DemoRetriever(demo_embs,demos,device='cuda:2')
 
@@ -13,9 +13,9 @@ query_id = np.random.randint(0,len(demos))
 logging.info('Query: %s'%query_id)
 query_emb = demo_embs[query_id]
 query = demos[query_id]
-logging.info('Query: %s'%query['question'])
+logging.info('Query: %s'%query['sentence'])
 
 retrieved_demos = demo_retriever.retrieve(query_emb,topk=5)
 logging.info('Retrieved demos:')
 for demo in retrieved_demos:
-    logging.info(demo['question'])
+    logging.info(demo['sentence'])
